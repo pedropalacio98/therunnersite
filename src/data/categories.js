d80c0a0235aef15d0f89c5654d43be001a2142cb
ppalacio <pedropalacioestrada@gmail.com>
2025-05-02 15:14:22 +0200

    First changes to page

---
+++ src/data/categories.js
@@ -0,0 +1,30 @@
+export const categories = [
+  {
+    slug: "marathon",
+    name: "Maratón",
+    image: "../images/icons/marathon.png",
+    description:
+      "La distancia reina de 42,195 km. Necesitas zapatillas con amortiguación, estabilidad y durabilidad.",
+  },
+  {
+    slug: "half-marathon",
+    name: "Media Maratón",
+    image: "../images/icons/halfMarathon.png",
+    description:
+      "21,097 km de pura resistencia. Aquí prima el equilibrio entre ligereza y amortiguación.",
+  },
+  {
+    slug: "10k",
+    name: "10K",
+    image: "../images/icons/10k.png",
+    description:
+      "Una carrera rápida de 10 km. Zapatillas ágiles y responsivas para ritmos altos.",
+  },
+  {
+    slug: "trail",
+    name: "Trail",
+    image: "../images/icons/trail.png",
+    description:
+      "Terrenos irregulares y senderos. Neumáticos agresivos y protección extra contra piedras.",
+  },
+];
